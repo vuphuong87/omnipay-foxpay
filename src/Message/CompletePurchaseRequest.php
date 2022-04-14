e5246319f3dd714ee8a2a6318d85c4e1f17da800
Khanh Nguyen <quockhahnguyen1997@gmail.com>
2022-04-14 11:11:05 +0700

    Build Order with key's order

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -8,7 +8,30 @@ class CompletePurchaseRequest extends AbstractRequest
 {
     public function getData(): array
     {
-        $order     = $this->httpRequest->request->all();
+        $request = $this->httpRequest->request->all();
+
+        $order = [
+            'version'        => $request['version'] ?? null,
+            'transaction_id' => $request['transaction_id'] ?? null,
+            'merchant_id'    => $request['merchant_id'] ?? null,
+            'terminal_id'    => $request['terminal_id'] ?? null,
+            'payment_method' => $request['payment_method'] ?? null,
+            'order_id'       => $request['order_id'] ?? null,
+            'amount'         => $request['amount'] ?? null,
+            'currency'       => $request['currency'] ?? null,
+            'brand_card'     => $request['brand_card'] ?? null,
+            'issue_place'    => $request['issue_place'] ?? null,
+            'result_code'    => $request['result_code'] ?? null,
+            'result'         => $request['result'] ?? null,
+            'message'        => $request['message'] ?? null,
+            'url_bill'       => $request['url_bill'] ?? null,
+            'token_schema'   => $request['token_schema'] ?? null,
+            'token_brand'    => $request['token_brand'] ?? null,
+            'token_number'   => $request['token_number'] ?? null,
+            'token'          => $request['token'] ?? null,
+            'signature'      => $request['signature'] ?? null,
+        ];
+
         $signature = $order['signature'];
 
         // remove it before compute signature
